1e0a83fa6446867d423822a64fb9c0fc7689ec45
Niuren.Zhu <niuren.zhu@icloud.com>
2025-05-15 15:36:18 +0800

    调整发布策略

--- compile_packages_test.bat
+++ compile_packages_test.bat
@@ -12,14 +12,18 @@ echo     4. 运行提示符运行mvn -v 检查安装是否成功。
 echo     5. 此脚本用于编译测试war包。
 echo     6. 报错时请尝试先运行compile_packages.bat。
 echo ****************************************************************************
-REM 设置参数变量
+rem 设置参数变量
 SET WORK_FOLDER=%~dp0
 
 echo --当前工作的目录是[%WORK_FOLDER%]
 if not exist %WORK_FOLDER%release md %WORK_FOLDER%release
-REM 编译测试包
+set MVN_BIN=mvn
+if "%MAVEN_HOME%" neq "" (
+  set MVN_BIN="%MAVEN_HOME%"\bin\mvn
+)
+rem 编译测试包
 if exist %WORK_FOLDER%ibas.integration.service\pom.test.xml (
-  call "%MAVEN_HOME%\bin\mvn" clean package -Dmaven.test.skip=true -f %WORK_FOLDER%ibas.integration.service\pom.test.xml 
+  call %MVN_BIN% clean package -Dmaven.test.skip=true -f %WORK_FOLDER%ibas.integration.service\pom.test.xml 
   if exist %WORK_FOLDER%ibas.integration.service\target\ibas.integration.test*.war copy /y %WORK_FOLDER%ibas.integration.service\target\ibas.integration.test*.war %WORK_FOLDER%release
 )
 
